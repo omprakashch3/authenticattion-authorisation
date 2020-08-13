74f240a3e4a7e1c0109ea33dcf1c5414510ac4ff
omprakashch3 <omprakashchautala3@gmail.com>
2020-08-13 18:02:31 +0530

    full setup

---
+++ models/user.js
@@ -0,0 +1,42 @@
+const Joi = require("joi");
+const mongoose = require("mongoose");
+
+const User = mongoose.model(
+  "User",
+  new mangoose.schema({
+    name: {
+      type: String,
+      required: true,
+      minlength: 5,
+      maxlength: 50,
+    },
+
+    email: {
+      type: String,
+      required: true,
+      minlength: 5,
+      maxlength: 250,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+      minlength: 5,
+      maxlength: 1024,
+      
+  })
+);
+
+function validateUser(user) {
+  const schema = {
+    name: Joi.string().min(5).max(50).required(),
+    email: Joi.string().min(5).max(250).required().email(),
+    password: Joi.string().min(5).max(1024).required(),
+  };
+
+  return Joi.validate(user, schema);
+}
+
+
+exports.User = User;
+exports.validate =validateUser;
